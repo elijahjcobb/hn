b98fe49d87b73653297c6e1e6e5d7f1aa32a4753
Elijah Cobb <elijah.cobb@vercel.com>
2022-06-28 17:22:05 -0700

    made a pwa and fixed pwa scaling on mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>HN - elijahcobb.com</title>
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="initial-scale=1,user-scalable=no,viewport-fit=cover" />
+      <meta name="description" content="A Hacker News Clone" />
+      <meta name="theme-color" content="#ff6600" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.svg" />
+      <link rel="apple-touch-icon" href="/icon-256x256.png"></link>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
